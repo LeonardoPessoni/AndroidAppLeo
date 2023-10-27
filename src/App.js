5e86f9c3f9d96a4713ae57b920a7dc5a57fc91ee
LeonardoPessoni <pessonileonardo@gmail.com>
2023-10-27 17:32:31 -0300

    geolocalização

---
+++ src/App.js
@@ -0,0 +1,6 @@
+import React from "react";
+import Routes from "./route.js";
+
+const App = () => <Routes/> 
+
+export default App;
---
+++ src/pages/maps.js
@@ -0,0 +1,71 @@
+import React, { useEffect, useState } from 'react';
+import MapView, { Marker } from 'react-native-maps';
+import { styles } from '../assets/css/css';
+import { View, Text, Image } from 'react-native';
+import {GooglePlacesAutocomplete} from 'react-native-google-places-autocomplete';
+
+const Maps = ({ route }) => {
+    const { lat, long } = route.params;
+    const [latitude, setLatitude] = useState(parseFloat(lat));
+    const [longitude, setLongitude] = useState(parseFloat(long));
+    const [destinoLat, setDestinoLat] = useState(null);
+    const [destinoLon, setDestinoLon] = useState(null);
+
+    useEffect(() => {
+        if (destinoLat !== null && destinoLon !== null) {
+            setLatitude(destinoLat);
+            setLongitude(destinoLon);
+        }
+    }, [destinoLat, destinoLon]);
+
+    console.log('lat', latitude);
+    console.log('long', longitude)
+
+    return (
+        <View style={styles.containerMaps}>
+            <MapView
+                style={styles.map}
+                showsUserLocation={true}
+                showsMyLocationButton={false}
+                toolbarEnabled={false}
+                region={{
+                    latitude,
+                    longitude,
+                    latitudeDelta: 0.007,
+                    longitudeDelta: 0.007,
+                }}>
+                <Marker
+                    coordinate={{
+                        latitude,
+                        longitude,
+                    }}
+                    title="Meu local"
+                    description="Localização atual"
+                />
+            </MapView>
+            <View style={styles.search}>
+                <Text style={styles.text}>Digite sua nova localização:</Text>
+                <GooglePlacesAutocomplete
+                    placeholder="Para onde vamos?"
+                    onPress={(data, details = null) => {
+                        setDestinoLat(details.geometry.location.lat);
+                        setDestinoLon(details.geometry.location.lng);
+                    }}
+                    query={{
+                        key: '',
+                        language: 'pt-br',
+                    }}
+                    enablePoweredByContainer={false}
+                    fetchDetails={true}
+                    styles={{ listView: { height: 100 } }}
+                />
+                <Image
+                    style={styles.imageMaps}
+                    source={require('../assets/image/google-maps-vetor.png')}
+                />
+            </View>
+        </View>
+    );
+};
+
+export default Maps;
